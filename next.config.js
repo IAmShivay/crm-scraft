c895932a4a734cc52ffe9e654dfd05dbd5ed02c4
Rahel Melese <richmelese21@gmail.com>
2025-09-01 12:19:22 +0300

    fix loading issue and  optimizations applied and Fixed webpack configuration errors

--- next.config.js
+++ next.config.js
@@ -7,9 +7,15 @@ const config = {
   },
   images: {
     unoptimized: false,
-    domains: [
-      "supabasekong-pgo8c80w04gcoo4w88kgsw0s.breaktheice.in",
-      "supabasekong-u0k8kkksk4k8ccw0ow0kg084.breaktheice.in",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'supabasekong-pgo8c80w04gcoo4w88kgsw0s.breaktheice.in',
+      },
+      {
+        protocol: 'https',
+        hostname: 'supabasekong-u0k8kkksk4k8ccw0ow0kg084.breaktheice.in',
+      },
     ],
     formats: ["image/avif", "image/webp"],
   },
@@ -25,6 +31,31 @@ const config = {
         "*.svg": ["@svgr/webpack"],
       },
     },
+    // Performance optimizations
+    webpackBuildWorker: true,
+    optimizePackageImports: ['@radix-ui/react-icons', 'lucide-react'],
+  },
+  // Performance optimizations
+  poweredByHeader: false,
+  generateEtags: false,
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Optimize bundle size
+    if (!dev && !isServer) {
+      config.optimization.splitChunks.chunks = 'all';
+      config.optimization.splitChunks.cacheGroups = {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      };
+    }
+    
+    return config;
   },
 };
 
